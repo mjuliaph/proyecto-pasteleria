7cfe690ddd539243356d4bd34900156e92f75f24
Julia Pedraza <julia.pedraza@minseg.gob.ar>
2023-10-22 16:04:53 -0300

    Aplicando API

---
+++ js/api.js
@@ -0,0 +1,35 @@
+// CONSUMO DE API
+let lon;
+let lat;
+let temperatura = document.querySelector(".temp");
+let resumen = document.querySelector(".summary");
+let locacion = document.querySelector(".location");
+const KELVIN = 273.15;
+
+
+window.addEventListener('load', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const API = "89cd84c888b22e54cdac823764009dbc";
+
+            const URL_base = "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=89cd84c888b22e54cdac823764009dbc";
+
+            fetch(URL_base)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log("Esta es la data", data);
+
+                    temperatura.textContent = Math.floor(data.main.temp - KELVIN) + "°C";
+                    resumen.textContent = data.weather[0].description;
+                    locacion.textContent = data.name + "," + data.sys.country;
+                });
+        });
+    }
+
+});
